56397a383c2cd906c87669c861dd2edfedd497a7
adiyafre <adiyafre@gmail.com>
2014-09-21 18:11:05 -0500

    control

--- control/select_control.php
+++ control/select_control.php
@@ -2,55 +2,98 @@
 
 		include '../model/config.php';
 
-		//$tag = $_POST["tag"];
+		$tag = $_POST["tag"];
 
+/*
 class q{
 	
 	private $qtext;
 	var $qdate;
 	var $qtags = [];
 
-	function __construct($q)
+	public function __construct($q)
     {
-        $this->$qtext = $q;
+        $this->qtext = $q;
     } 
 
 
-	function tag_add($tag){
+	public function tag_add($tag){
 		array_push($this->$qtags,$tag);
 	}
 
 }
 
+*/
 
-		$tag = "phone";
 
-		$row_array = [];
+
+		
+
+		//$row_array = [];
 
 
 			$rows = $api->click_tag($tag);
 			$rows_q = $api->get_question($tag);
 
-			foreach ($rows_q as $row_q) {
-				$temp = new q($row_q);
+			include("../view/question_view.html");
+
+			/*foreach ($rows_q as $row_q) {
+				$temp = new q($row_q["question"]);
 				array_push($row_array, $temp);
 			}
 
 
+
+			//question1 question2
+			//tag1 tag2 tag1
+			/*$shop = array( array("rose", 1.25 , 15),
+               array("daisy", 0.75 , 25),
+               array("orchid", 1.15 , 7) 
+             ); /*
+             //$new_row = array($row["datasource_id"]=>$row["title"]); 
+			$tagarray = [];
+			$temp = [];
+			foreach($rows as $key => $value)
+			{
+  			$mykey = $key;
+  			$myq = $value["question"];
+  			$myt = $value["tag"];
+  			//echo $mykey . ":" . $myq . ":" . $myt;
+
+  			//$new_row = array($myq => $myt);
+  			//array_push($tagarray, $new_row);
+
+
+  			//array_push($temp, $myt);
+  			//$tagarray[$myq] = $temp;
+
+  			
+
+
+
+  			//["question1", "tag1,tag2,tag3", "date1"];
+  			//["question2", "tag1, tag2", "date2"];
+
+
+
+  			}
+
+			//var_dump($tagarray);
+			
+
+
 			if(isset($rows) && $rows->rowCount() > 0){
 				foreach($rows as $row){
-					//echo $row["tag"].":".$row["question"].":".$row["added"]."<br>";
-					echo $row["question"];
+					
+				
 					$question = $row["question"]; $date = $row["added"];
 					
 									
 				}
 			}
-			else{
-				print "not found";
-				return FALSE;
+			
 		}
 
-
+			/*
 
 ?>
