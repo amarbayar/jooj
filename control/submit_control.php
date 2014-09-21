3c2847827ff2b5a79fa3c9b9e28631e9c4b4d827
adiyafre <adiyafre@gmail.com>
2014-09-20 21:26:59 -0500

    new control files
    update control files

--- control/submit_control.php
+++ control/submit_control.php
@@ -5,27 +5,34 @@ require_once('connect.php');
 		//$tags = $_POST["tags"];
 		
 		//$question = $_POST["iquestion"];
+
+
+		//$type = $_POST["itype"];
+		$type = "skype";
 		
 		$tags = array("google", "microsoft", "infosys", "logic");
 		//$tags = array("micros");
-		var_dump($tags);
-		
+		array_push($tags,$type);
+		//var_dump($tags);
+
 		$question = "how much snow drops in Seatle";
 		mysqli_query($con,"INSERT INTO questions (question ) VALUES ('$question')");
 		$qid = $con->insert_id;
-		echo $qid;
+		//echo $qid;
+
+		
 
 		foreach ($tags as $tag) {
 
 			$tagExist = findTag($tag);
 			if ($tagExist>0) {
 
-			$tid = $tagExist; echo $tid . "!!!";
+			$tid = $tagExist; 
 
 			} else {
 
 			mysqli_query($con,"INSERT INTO tags (tag) VALUES ('$tag')");
-			$tid = $con->insert_id; echo $tid . "&&&";
+			$tid = $con->insert_id; 
 			}
 			
 			
@@ -42,7 +49,7 @@ function findTag($tag){
 	
 	require('connect.php');
 	if ($result = $con->query("SELECT tid from tags WHERE tag = '$tag'")){
-		$row = mysqli_fetch_array($result); echo $row['tid'] . "TID";
+		$row = mysqli_fetch_array($result); 
 		return $row['tid']; 
 		}
 		else return 0;
