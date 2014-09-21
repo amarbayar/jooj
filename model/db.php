5a143d2c93dc3424562151332f67fe580585bffc
AmarboldS <s_amarbold@yahoo.com>
2014-09-20 20:42:45 -0500

    adding model section for DB calls

---
+++ model/db.php
@@ -0,0 +1,138 @@
+<?php
+
+if (stristr($_SERVER['PHP_SELF'], "db.php")) {
+    Header("Location: ../index.php");
+    die();
+}
+
+$dbhost = "localhost";
+$dbuname = "root";
+$dbpass = "";
+$dbname = "jooj";
+$prefix = "jo";
+
+class sql_db
+{
+
+	var $db_connect_id;
+	var $query_result;
+	var $row = array();
+	var $rowset = array();
+	var $num_queries = 0;
+
+	//
+	// Constructor
+	//
+	function sql_db($sqlserver, $sqluser, $sqlpassword, $database, $persistency = true)
+	{
+
+		$this->persistency = $persistency;
+		$this->user = $sqluser;
+		$this->password = $sqlpassword;
+		$this->server = $sqlserver;
+		$this->dbname = $database;
+
+		if($this->persistency)
+		{
+			$this->db_connect_id = @mysql_pconnect($this->server, $this->user, $this->password);
+		}
+		else
+		{
+			$this->db_connect_id = @mysql_connect($this->server, $this->user, $this->password);
+		}
+		if($this->db_connect_id)
+		{
+			if($database != "")
+			{
+				$this->dbname = $database;
+				$dbselect = @mysql_select_db($this->dbname);
+				if(!$dbselect)
+				{
+					@mysql_close($this->db_connect_id);
+					$this->db_connect_id = $dbselect;
+				}
+			}
+			return $this->db_connect_id;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	//
+	// Other base methods
+	//
+	function sql_close()
+	{
+		if($this->db_connect_id)
+		{
+			if($this->query_result)
+			{
+				@mysql_free_result($this->query_result);
+			}
+			$result = @mysql_close($this->db_connect_id);
+			return $result;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	//
+	// Base query method
+	//
+	function sql_query($query = "", $transaction = FALSE)
+	{
+		// Remove any pre-existing queries
+		unset($this->query_result);
+		if($query != "")
+                {
+
+			$this->query_result = @mysql_query($query, $this->db_connect_id);
+
+		}
+		if($this->query_result)
+		{
+			unset($this->row[$this->query_result]);
+			unset($this->rowset[$this->query_result]);
+			return $this->query_result;
+		}
+		else
+		{
+			return ( $transaction == END_TRANSACTION ) ? true : false;
+		}
+	}
+
+	function sql_fetchrow($query_id = 0)
+	{
+		if(!$query_id)
+		{
+			$query_id = $this->query_result;
+		}
+		if($query_id)
+		{
+			$this->row[$query_id] = @mysql_fetch_array($query_id);
+			return $this->row[$query_id];
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	function get_tag()
+	{
+		global $prefix;
+		$row = self::sql_fetchrow(self::sql_query("SELECT tag FROM ".$prefix."_tags"));
+		return $row;
+	}
+}
+
+
+$db = new sql_db($dbhost, $dbuname, $dbpass, $dbname, false);
+if(!$db->db_connect_id) {
+    die("<br><br><center><img src=images/logo.gif><br><br><b>There seems to be a problem with the $dbtype server, sorry for the inconvenience.<br><br>We should be back shortly.</center></b>");
+}
+?>
