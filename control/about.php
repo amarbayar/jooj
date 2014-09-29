1ff6e6d09b85d01dc006b1f40cf3ce3ca2b1d962
Amarbayar <amarbayar.amarsanaa@gmail.com>
2014-09-29 01:24:45 -0500

    added input validation using jquery, updated some design work to header and footer,  and positionings of questions and tags

---
+++ control/about.php
@@ -0,0 +1,5 @@
+<?php
+	include("../view/header_view.html");
+	include("../view/about.html");
+	include("../view/footer_view.html");
+?>
---
+++ control/contact.php
@@ -0,0 +1,5 @@
+<?php
+	include("../view/header_view.html");
+	include("../view/contact.html");
+	include("../view/footer_view.html");
+?>
